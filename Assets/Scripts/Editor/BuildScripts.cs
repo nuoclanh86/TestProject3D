65b35c45578effb3d2e252f731bc0fb19cb1f092
nuoclanh86 <nuoclanh86@gmail.com>
2022-06-21 20:44:49 +0700

    - need to put the scripts used for building into a folder named Editor
    need to put the scripts used for building into a folder named Editor (specifically, ./Assets/Scripts/Editor)

--- Assets/Scripts/Editor/BuildScripts.cs
+++ Assets/Scripts/Editor/BuildScripts.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class BuildScripts : MonoBehaviour
+public class BuildScripts
 {
     public static void PerformBuildApk()
     {
