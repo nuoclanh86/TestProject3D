7c8601f7648fe5cf9cbccf92f196b1473c1eec12
nuoclanh86 <nuoclanh86@gmail.com>
2022-06-22 06:41:41 +0700

    - add PerformBuild

--- Assets/Scripts/Editor/BuildScripts.cs
+++ Assets/Scripts/Editor/BuildScripts.cs
@@ -16,4 +16,11 @@ public class BuildScripts
     {
         EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
     }
+
+	public static void PerformBuild ()
+	{
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = new[] { "Assets/Scene1.unity", "Assets/Scene2.unity" };
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+	}
 }
--- BuildScripts/BuildApk.sh
+++ BuildScripts/BuildApk.sh
@@ -2,9 +2,12 @@ echo Clearning Up Build Directory
 rm -rf ../build/
 echo NuocLanh : Starting Build Process
 echo NuocLanh : PerformSwitchAndroid
-# '/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformSwitchAndroid
-'/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformSwitchAndroid
+'/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformSwitchAndroid
+# '/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformSwitchAndroid
 echo NuocLanh : PerformBuildApk
-# '/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformBuildApk
-'/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformBuildApk
+'/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformBuildApk
+# '/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformBuildApk
+echo NuocLanh : PerformBuild
+'/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformBuild
+# '/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath ../ -executeMethod BuildScripts.PerformBuild
 echo NuocLanh : Done
