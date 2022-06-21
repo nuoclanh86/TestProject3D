fb77cc859eec3b17b143d9b875ba187b0e505a87
nuoclanh86 <nuoclanh86@gmail.com>
2022-06-21 21:08:48 +0700

    Update BuildApk.sh

--- BuildScripts/BuildApk.sh
+++ BuildScripts/BuildApk.sh
@@ -2,7 +2,9 @@ echo Clearning Up Build Directory
 rm -rf ../build/
 echo NuocLanh : Starting Build Process
 echo NuocLanh : PerformSwitchAndroid
-'/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -bachmode -projectPath ../ -executeMethod BuildScripts.PerformSwitchAndroid
+# '/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -bachmode -projectPath ../ -executeMethod BuildScripts.PerformSwitchAndroid
+'/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity' -quit -bachmode -projectPath ../ -executeMethod BuildScripts.PerformSwitchAndroid
 echo NuocLanh : PerformBuildApk
-'/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -bachmode -projectPath ../ -executeMethod BuildScripts.PerformBuildApk
+# '/c/Program Files/Unity/2020.3.24f1/Editor/Unity.exe' -quit -bachmode -projectPath ../ -executeMethod BuildScripts.PerformBuildApk
+'/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity' -quit -bachmode -projectPath ../ -executeMethod BuildScripts.PerformBuildApk
 echo NuocLanh : Done
