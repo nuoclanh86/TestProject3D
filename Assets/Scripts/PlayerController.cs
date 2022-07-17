b03efd48cd81ee86918871e194fcb275a2e76319
Tung Nguyen Thanh <nuoclanh86@gmail.com>
2022-07-17 20:15:41 +0700

    - rename local player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,32 +9,39 @@ public class PlayerController : NetworkBehaviour
     public float m_moveSpeed = 5.0f;
     Vector3 m_moveDir = Vector3.zero;
 
+    public static Dictionary<ulong, PlayerController> Players = new Dictionary<ulong, PlayerController>();
+
     public override void OnNetworkSpawn()
     {
-        if (!IsOwner) Destroy(this);
+        //if (!IsOwner) Destroy(this);
+        Players[OwnerClientId] = this;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        if (NetworkManager.Singleton.IsServer)
+        if (IsLocalPlayer)
         {
-            this.gameObject.name = "PlayerServer";
-            m_txtName.text = "PlayerServer";
+            if (NetworkManager.Singleton.IsServer)
+            {
+                this.gameObject.name = "PlayerServer";
+                m_txtName.text = "PlayerServer";
+            }
+            else
+            {
+                this.gameObject.name = "PlayerClient";
+                m_txtName.text = "PlayerClient";
+            }
+            m_moveDir = new Vector3(Random.Range(-5.0f, 5.0f), 0.0f, Random.Range(-5.0f, 5.0f));
+            this.transform.position = m_moveDir;
         }
-        else
-        {
-            this.gameObject.name = "PlayerClient";
-            m_txtName.text = "PlayerClient";
-        }
-        m_moveDir = new Vector3(Random.Range(-5.0f, 5.0f), 0.0f, Random.Range(-5.0f, 5.0f));
-        this.transform.position = m_moveDir;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += m_moveDir * Time.deltaTime * m_moveSpeed;
+        if (IsLocalPlayer)
+            transform.position += m_moveDir * Time.deltaTime * m_moveSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
