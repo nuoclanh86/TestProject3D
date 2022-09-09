225b606df018257308e3629e2c6a432cfc29f7b0
Tung Nguyen Thanh <nuoclanh86@gmail.com>
2022-09-09 21:12:30 +0700

    - add player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float speed = 1.0f;
+    public new TextMesh name;
+    Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string nametxt = "Player0" + (int)Random.Range(0.0f, 99.0f);
+        SetNameClientRpc(nametxt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (IsOwner)
+        //{
+        move = Vector3.zero;
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+        this.transform.position += move * speed;
+        //}
+        //else
+        //    this.GetComponent<PlayerController>().enabled = false;
+    }
+
+    [ClientRpc]
+    public void SetNameClientRpc(string nametxt)
+    {
+        SetName(nametxt);
+    }
+
+    public void SetName(string nametxt)
+    {
+        name.text = nametxt;
+        this.gameObject.name = nametxt;
+    }
+}
